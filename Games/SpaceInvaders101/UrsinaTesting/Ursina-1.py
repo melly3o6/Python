acdf515c5d27ac64f08177a43e2bd149ddd3af17
zkampm <zkampm@bbcag.ch>
2023-11-15 12:50:52 +0100

    All Python Code will be done in this Project

---
+++ Games/SpaceInvaders101/UrsinaTesting/Ursina-1.py
@@ -0,0 +1,66 @@
+#############################################################
+# Import Libraries
+
+from ursina import *
+#############################################################
+# Initialize Libraries
+app = Ursina()
+
+#############################################################
+# Define Constant Variables
+
+current_color = color.hex('#A06090')
+
+CUBE = Entity(model="cube", color=current_color, scale=(0.5, 0.5, 2))
+
+#############################################################
+# Images
+#############################################################
+# Create Game Window
+
+#############################################################
+# Fonts
+#############################################################
+# Create Game Scenes
+#############################################################
+# Sprite Classes
+#############################################################
+# Logics
+
+
+def update():
+
+    # ROTATE CONTROLS
+
+    if held_keys['w']:
+        CUBE.rotation_x += 1
+
+    if held_keys['s']:
+        CUBE.rotation_x -= 1
+
+    if held_keys['a']:
+        CUBE.rotation_y += 1
+
+    if held_keys['d']:
+        CUBE.rotation_y -= 1
+
+    # MOVE CONTROLS
+
+    if held_keys['right arrow'] and CUBE.x < 7.0:
+        CUBE.x += 1
+    if held_keys['left arrow'] and CUBE.x > -7.0:
+        CUBE.x -= 1
+    if held_keys['up arrow'] and CUBE.y < 4.0:
+        CUBE.y += 1
+    if held_keys['down arrow'] and CUBE.y > -4.0:
+        CUBE.y -= 1
+
+    print(f"POSITION: X = {CUBE.x} Y = {CUBE.y} | ROTATION: X = {CUBE.rotation_x} Y = {CUBE.rotation_y}")
+
+#############################################################
+# Main Loop
+
+app.run()
+
+#############################################################
+# Help Links --->
